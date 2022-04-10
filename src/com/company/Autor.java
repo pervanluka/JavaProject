4c4620faedb09ca1cd145431b49174e6c593fec7
Luka Pervan <lukapervan@hotmail.com>
2022-04-11 00:51:55 +0200

    Biblioteka kroz Listu podataka: * kreairanje autora * ucitavanje svih autora * uklanjanje autora po naslovu knjige
    UPDATE(11.04.2022):
    *omogucen unos i citanje iz datoteke

--- src/com/company/Autor.java
+++ src/com/company/Autor.java
@@ -1,10 +1,11 @@
 package com.company;
 
-import java.io.Serializable;
+import java.io.*;
+import java.util.*;
 
 public class Autor implements Serializable{
 
-    final private static String filepath = "/Users/lukapervan/IdeaProjects/JavaPractice/Knjige.txt";
+    final private static File filePath = new File("/Users/lukapervan/IdeaProjects/JavaPractice/Knjige.txt");
 
     private String imeAutora;
     private String prezimeAutora;
@@ -60,6 +61,105 @@ public class Autor implements Serializable{
     public String toString(){
         return "Ime: " + this.imeAutora + "\nPrezime: " + this.prezimeAutora + "\nKnjiga: " + this.naslovKnjige +
                 "\nZarn: " + this.zarnKnjige + "\n\n";
+    }
+
+    public void unos(Autor a){
+        final List<Autor> autori = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Unesite ime autora:");
+        String imeAutora = sc.nextLine();
+
+        System.out.println("Unesite prezime autora:");
+        String prezimeAutora = sc.nextLine();
+
+        System.out.println("Unesite naslov knjige:");
+        String naslovKnjige = sc.nextLine();
+
+        System.out.println("Unesite zarn knjige:");
+        String zarnKnjige = sc.nextLine();
+
+        a = new Autor(imeAutora, prezimeAutora, naslovKnjige, zarnKnjige);
+        autori.add(a);
+
+        System.out.println("Zelite li unijeti jos autora?(Da/Ne)");
+        String odg = sc.nextLine().toLowerCase();
+
+        if(odg.equals("da"))
+            unos(new Autor());
+        else if(odg == "ne"){
+            try {
+                FileOutputStream fos = new FileOutputStream(filePath);
+                ObjectOutputStream oos = new ObjectOutputStream(fos);
+                if(!filePath.exists())
+                    filePath.createNewFile();
+                oos.writeObject(autori);
+                oos.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("U meni-u ste.");
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("U meni-u ste.");
+            }
+
+        }
+
+    }
+
+    public void ispisAutora(){
+        try {
+            if(!filePath.exists())
+                System.out.println("Lista je prazna!");
+            else if(filePath.canRead()) {
+                FileInputStream fin = new FileInputStream(filePath);
+                ObjectInputStream ois = new ObjectInputStream(fin);
+                ArrayList<Autor> Objekti1 = (ArrayList<Autor>) ois.readObject();
+                for (Autor o : Objekti1)
+                    System.out.println(o);
+            }
+            else
+                System.out.println("Ne moze ucitati datoteku!");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void brisi(String naslov){
+        try {
+            FileInputStream fin = new FileInputStream("Example.txt");
+            ObjectInputStream ois = new ObjectInputStream(fin);
+            ArrayList<Autor> Objekti1 = (ArrayList<Autor>) ois.readObject();
+
+            if(Objekti1.isEmpty())
+                System.out.println("Lista je prazna!");
+
+            else {
+                Iterator<Autor> iter = Objekti1.iterator();
+
+                while (iter.hasNext()) {
+                    Autor autor1 = iter.next();
+
+                    if (autor1.getNaslovKnjige().equals(naslovKnjige)) {
+                        iter.remove();
+                        System.out.println("Uspjesno uklonjeno!");
+                    }
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     /*public void save(ArrayList<Autor> autori) throws FileNotFoundException {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,8 @@ class Main{
 
     //MAIN
     public static void main(String[] args) {
-        List<Autor> autori = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        Autor autor = new Autor("Luka", "Pervan", "Stranac", "Drama");
-        autori.add(autor);
+        final Autor a = new Autor();
 
         while(true){
 
@@ -20,84 +18,32 @@ class Main{
                     "Opcija 3: Brisanje knjige:\n" +
                     "Opcija 4: Izlaz");
             int opcija = scanner.nextInt();
-            if(opcija < 0 && opcija > 6){
+            if(opcija < 0 && opcija > 4){
                 System.out.println("Unijeli ste broj koji ne nudi uslugu, pokusajte ponovo:");
             }
             else if(opcija == 1){
-
-                if(autori.isEmpty()) {
-                    System.out.println("Lista je prazna!");
-                    break;
-                }
-
-                //PODACI
-                for(Autor a: autori){
-                    System.out.println(a);
-                }
+                //ISPIS AUTORA
+                a.ispisAutora();
             }
             else if(opcija == 2){
 
                 //STVARANJE AUTORA
-                Scanner sc = new Scanner(System.in);
+                Autor autor1 = new Autor();
+                a.unos(autor1);
 
-                System.out.println("Unesite ime autora:");
-                String imeAutora = sc.nextLine();
 
-                System.out.println("Unesite prezime autora:");
-                String prezimeAutora = sc.nextLine();
-
-                System.out.println("Unesite naslov knjige:");
-                String naslovKnjige = sc.nextLine();
-
-                System.out.println("Unesite zarn knjige:");
-                String zarnKnjige = sc.nextLine();
-
-                Autor a = new Autor(imeAutora, prezimeAutora, naslovKnjige, zarnKnjige);
-                autori.add(a);
-                System.out.println("Uspjesno dodano!");
             }
             else if(opcija == 3){
-                System.out.println("Unesite naslov knjige koju zelite izbrisati:");
+                /*System.out.println("Unesite naslov knjige koju zelite izbrisati:");
                 Scanner sc = new Scanner(System.in);
                 String naslovKnjige = sc.nextLine();
-                if(autori.isEmpty()) {
-                    System.out.println("Lista je prazna!");
-                    break;
-                }
-                Iterator<Autor> iter = autori.iterator();
-
-                while (iter.hasNext()) {
-                    Autor autor1 = iter.next();
-
-                    if (autor1.getNaslovKnjige().equals(naslovKnjige)) {
-                        iter.remove();
-                        System.out.println("Uspjesno uklonjeno!");
-                    }
-                }
+                a.brisi(naslovKnjige);*/
 
             }
             else if(opcija == 4){
                 System.out.println("Izasli ste!");
                 break;
             }
-            /*else if(opcija == 5){
-                try (FileOutputStream fos = new FileOutputStream("object.dat");
-                     ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-
-                    // create a new user object
-                    Autor user = new Autor();
-
-                    // write object to file
-                    oos.writeObject(user);
-
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-
-
-            }*/
         }
-
-
     }
 }
